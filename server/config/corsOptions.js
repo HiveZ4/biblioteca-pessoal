bc0793881a0bf373cd59ac31485beb37e5d7e549
Vicenzo Goulart <vicenzogoulart@gmail.com>
2025-07-08 17:49:41 -0300

    commit

--- server/config/corsOptions.js
+++ server/config/corsOptions.js
@@ -4,7 +4,7 @@ const allowedOrigins = [
   'http://127.0.0.1:3000',
   'http://127.0.0.1:3001',
   "https://biblioteca-pessoal-eta.vercel.app/",
-  
+  "https://vercel.com/hivez4s-projects/biblioteca-pessoal/EQuqyhzG2garUY2k4HNZCrBvqT8r",
 ];
 
 const corsOptions = {
