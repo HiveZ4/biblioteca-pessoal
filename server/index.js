c6e89bf4c76b37bf156f64ee104a5f3b0aed2016
Vicenzo Goulart <vicenzogoulart@gmail.com>
2025-07-08 21:04:38 -0300

    commit

--- server/index.js
+++ server/index.js
@@ -2,9 +2,11 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const corsOptions = {
-  origin:"*",
+  origin:["https://biblioteca.pessoal.pdt.vercel.app",
+    "https://biblioteca.pessoal.eta.vercel.app"],
   credential: true,
-  methods: ["GET", "POST", "PUT", "DELETE"],
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"]
 };
 const PORT = process.env.PORT || 8082;
 
